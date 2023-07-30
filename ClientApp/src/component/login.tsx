afde567229a9ab2f6e84fd623ac712c03d5b9327
Admin <Admin@DESKTOP-KQDDH8S>
2023-07-31 04:25:23 +0700

    update

--- ClientApp/src/component/login.tsx
+++ ClientApp/src/component/login.tsx
@@ -1,10 +1,18 @@
+import AuthSvc from "../services/authSvc";
+
+
+
 export default function Login() {
     function Login(){
-            alert('abc');
+           AuthSvc.login('phuocnguyen', '123456').then(result => {
+            debugger;
+           });
     }
     return (
         <div className="container">
             <div className="login-form">
+                <div className="form-logo">
+                </div>
                 <label>Tên Đăng Nhập
                     <input className="form-control"></input>
                 </label>
---
+++ ClientApp/src/services/authSvc.tsx
@@ -0,0 +1,26 @@
+import  axios  from "axios";
+
+// export default class AuthSvc {
+ 
+//     constructor(){
+
+//     }
+
+//   public Login(userName: string, password: string){
+//        axios.post('/api/user/login', { 
+//               userName: userName,
+//               lastName: password
+//             });
+//     }
+// }
+
+const AuthSvc = {
+    login : function(userName: string, password: string){
+       return axios.post('https://localhost:7179/api/user/login', { 
+                          userName: userName,
+                          lastName: password
+                        });
+    }
+};
+
+export default AuthSvc;
